2f22455bab0d25dd1386574c609a2674461994d9
truptilingaraju <trupti.l01@testyantra.in>
2024-08-07 14:37:52 +0530

    first commit

---
+++ src/main/java/com/ty/qa_env/qa_env/QaEnvApplication.java
@@ -0,0 +1,13 @@
+package com.ty.qa_env.qa_env;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaEnvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaEnvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ty/qa_env/qa_env/QaEnvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.qa_env.qa_env;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaEnvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
